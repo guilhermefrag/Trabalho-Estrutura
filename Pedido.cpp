3015080d019dcffd3d881d84fc4f995cb941caf1
guilhermefrag <guilhermefragnani@icloud.com>
2022-06-25 23:58:49 -0300

    Subindo

--- Pedido.cpp
+++ Pedido.cpp
@@ -2,8 +2,10 @@
 #include <queue>
 using namespace std;
 
+//LISTA DO TIPO FIFO
+
 class Pedido{
-   private:
+   public:
         int codPedido;
         int codLanche;
         float valor;
@@ -16,14 +18,18 @@ class Pedido{
           this->nomeCliente = nomeCliente;
           this->valor = 0;
       }
+      Pedido(){ //se precisar inicializar em branco
+            this->codLanche = 0;
+            this->descricao = "";
+            this->nomeCliente = "";
+            this->valor = 0;
+      }
       
-
-      ~Pedido() {
-        cout << "Object Destroyed" << endl;
+      ~Pedido() { //Destruidor ???????????? só deixar em branco
       }
 
-      void print() {
-         cout << (codLanche ) << endl;
+      void print() { //isso a gente nem ta usando
+         cout << (codLanche, valor, descricao, nomeCliente) << endl;
       }
 };
 
@@ -32,34 +38,148 @@ void imprimeMenu(string cardapio[]){
     for(int i = 0; i < 10; i++){
         cout << i << "-" << cardapio[i] << endl;
     }
+    cout << "---------------Opções--------------" << endl;
+    cout << "10 - Listar Pedidos" << endl;
+    cout << "11 - Faturar Pedido" << endl;
+    cout << "12 - Quantidade de Pedidos" << endl;
+    cout << "13 - Ultimo Pedido Feito" << endl;
+    cout << "---------------------------------";
+}
+
+void imprimePedido(queue<Pedido> pedidos){
+    cout << "---------Pedidos---------" << endl;
+    int codigoPedido;
+    int codigoLanche;
+    float valorPedido;
+    string descricaoPedido;
+    string nomeClientePedido;
+
+    while(!pedidos.empty()){
+        codigoPedido = pedidos.front().codPedido;
+        codigoLanche = pedidos.front().codLanche; 
+        valorPedido = pedidos.front().valor; 
+        descricaoPedido = pedidos.front().descricao; 
+        nomeClientePedido = pedidos.front().nomeCliente; 
+        cout << "Codigo do pedido: " << codigoPedido << endl;
+        cout << "Codigo do lanche: " << codigoLanche << endl;
+        cout << "Valor do pedido: " << valorPedido << endl;
+        cout << "Descricao do pedido: " << descricaoPedido << endl;
+        cout << "Nome do cliente: " << nomeClientePedido << endl;
+        pedidos.pop();
+        cout << "---------------------------------" << endl;
+    }
+}
+
+void criaPedido(queue<Pedido> &pedidos,string cardapio[], int codLanche, float valor){
+    Pedido pedido;
+    pedido.descricao = cardapio[codLanche];
+    cout << "Digite o nome do cliente: ";
+    cin >> pedido.nomeCliente;
+    pedido.codPedido = pedidos.size()+1;
+    pedidos.push(pedido);
+}
+
+void quantiadePedido(queue<Pedido> &pedidos){
+    cout << "Quantidade de pedidos: " << pedidos.size() << endl;
+}
+
+void faturaPedido(queue<Pedido> &pedido){
+    pedido.pop();
+}
+
+void retornaUltimo(queue<Pedido> &pedidos){
+    cout << "---------------------------------";
+    cout << "Ultimo pedido: " << endl;
+    cout << "Codigo do pedido: " << pedidos.back().codPedido << endl;
+    cout << "Codigo do lanche: " << pedidos.back().codLanche << endl;
+    cout << "Valor do pedido: " << pedidos.back().valor << endl;
+    cout << "Descricao do pedido: " << pedidos.back().descricao << endl;
+    cout << "Nome do cliente: " << pedidos.back().nomeCliente << endl;
+    cout << "---------------------------------";
+}
+
+void retornaPedido(queue<Pedido> &pedidos){
+    int codigoPedido;
+    
+    cout << "Digite o codigo do pedido: ";
+    cin >> codigoPedido;
+    for(int i = 0; i < pedidos.size(); i++){
+        if(pedidos.front().codPedido == codigoPedido){
+            pedidos.pop();
+        }
+    }
 }
 
 int main() {
    // FIFO
-   string cardapio[10] = {"Sair","X-Burguer", "X-Bacon", "X-Egg", "X-Galinha", "X-GalinhaBacon", "X-Tudo", "X-Casa","X-Calabresa", "X-Coração"};
+   string cardapio[10] = {"Sair","X-Burguer", "X-Bacon", "X-Egg", "X-Galinha", "X-Galinha Bacon", "X-Tudo", "X-Casa","X-Calabresa", "X-Coração"};
    queue<Pedido> pedidos;
-   pedidos.push(Pedido(2, 3.8,"X-Burguer", "Pankaj"));
-
+   cout << cardapio[1] << endl;
    int opcao;
 
     do{
         imprimeMenu(cardapio);
         cin >> opcao;
         switch(opcao){
+            //Sair
             case 0:
                 cout << "Saindo..." << endl;
                 break;
+            //Burguer
             case 1:
-                pedidos.push(Pedido(opcao,3.8,cardapio[opcao],"Jefersson"));
+                criaPedido(pedidos,cardapio, opcao, 10.0);
+                break;
+            //Bacon
+            case 2:
+                criaPedido(pedidos,cardapio, opcao, 13.0);
+                break;
+            //Egg
+            case 3:
+                criaPedido(pedidos,cardapio, opcao, 12.0);
+                break;
+            //Galinha
+            case 4:
+                criaPedido(pedidos,cardapio, opcao, 11.0);
+                break;
+            //GalinhaBacon
+            case 5:
+                criaPedido(pedidos,cardapio, opcao, 14.0);
+                break;
+            //Tudo
+            case 6:
+                criaPedido(pedidos,cardapio, opcao, 15.0);
+                break;
+            //Casa
+            case 7:
+                criaPedido(pedidos,cardapio, opcao, 20.0);
+                break;
+            //Calabresa
+            case 8:
+                criaPedido(pedidos,cardapio, opcao, 17.0);
+                break;
+            //Coração
+            case 9:
+                criaPedido(pedidos,cardapio, opcao, 18.0);
+                break;
+            //Listar Pedidos
+            case 10:
+                imprimePedido(pedidos);
+                break;
+            //Faturar Pedido(Primeiro da Lista)
+            case 11:
+                cout << "Faturando Pedido...." << endl;
+                faturaPedido(pedidos);
+                break;
+            //Quantidade de Pedidos
+            case 12:
+                quantiadePedido(pedidos);
+                break;
+            //Ultimo Pedido Feito
+            case 13:
+                retornaUltimo(pedidos);
                 break;
         }
     }while(opcao != 0);
 
-   // Traverse over queue
-   while(pedidos.size()) {
-      pedidos.front().print();
-      pedidos.pop();
-   }
-
-   return 0;
+    return 0;
 }
